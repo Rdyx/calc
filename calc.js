5aab1a6ff7c60c3c32d2b63741f3701ab65a515d
Rdyx <tiranico.pro@gmail.com>
2017-07-04 15:25:15 +0200

    Have Fun !

--- calc.js
+++ calc.js
@@ -1,7 +1,39 @@
-var nombre1 = Number(prompt("Dis moi un chiffre"));
+/*var nombre1 = Number(prompt("Dis moi un chiffre"));
 var nombre2 = Number(prompt("Dis moi un autre chiffre"));
-var resultatadd = nombre1 + nombre2;
-var resultatmult = nombre1 * nombre2;
+var resultatAdd = nombre1 + nombre2;
+var resultatMult = nombre1 * nombre2;
 
-alert("Le résultat fait " + resultatadd + " ou si on multiplie " + resultatmult);
-//alert("Le résultat fait " + .val(nombre1+nombre2) + nombre1*nombre2);
+alert("Le résultat fait " + resultatAdd + " ou si on multiplie " + resultatMult);*/
+
+alert("On va voir si je sais compter Ramoucho")
+
+function addition(test1, test8Y38Y2TU)
+{
+
+	var result;
+	result = test1 + test8Y38Y2TU;
+	return alert(result + ", C'est bien Roukmouto !");
+
+}
+
+var nombre1 = Number(prompt("Dis moi un chiffre"));
+var nombre2 = Number(prompt("Bien Bonboula, maintenant un autre !"));
+var total = addition(nombre1, nombre2);
+		
+alert("Je sais même multiplier !")
+var myImg = new Image();
+function multiplication(cocobongo, nestcepas)
+{
+
+	var totalmodafucka;
+	totalmodafucka = cocobongo * nestcepas;
+	return alert(totalmodafucka + ", JEANNE, AU SECOURS !");
+}
+
+
+
+var trouloulou = Number(prompt("Envoie le 1er chiffre Akim le voleur"));
+var bidule = Number(prompt("Et le 2eme ? J't'ai vu t'as essayé de le voler !"));
+var KOMBIENCAFE = multiplication(trouloulou, bidule);
+
+//P.S : CECI EST UN TEST ET N'EST EN AUCUN CAS http://24.media.tumblr.com/tumblr_m7maxr4fUe1rq3b5ko1_400.gif
