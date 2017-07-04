5ca20a3a63c048bd3f1f6ce23a8d02928715824d
Rdyx <tiranico.pro@gmail.com>
2017-07-04 12:19:30 +0200

    JS Ok Number

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
-var nombre1 = prompt("Dis moi un chiffre");
-var nombre2 = prompt("Dis moi un autre chiffre");
-var resultatadd = nombre1*1 + nombre2*1;
+var nombre1 = Number(prompt("Dis moi un chiffre"));
+var nombre2 = Number(prompt("Dis moi un autre chiffre"));
+var resultatadd = nombre1 + nombre2;
 var resultatmult = nombre1 * nombre2;
 
 alert("Le résultat fait " + resultatadd + " ou si on multiplie " + resultatmult);
