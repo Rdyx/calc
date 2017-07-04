89168daa1af2f86b67f9adf9742538fc2341fb7f
Rdyx <tiranico.pro@gmail.com>
2017-07-04 11:43:27 +0200

    Js calculatrice OK

---
+++ calc.js
@@ -0,0 +1,7 @@
+var nombre1 = prompt("Dis moi un chiffre");
+var nombre2 = prompt("Dis moi un autre chiffre");
+var resultatadd = nombre1*1 + nombre2*1;
+var resultatmult = nombre1 * nombre2;
+
+alert("Le résultat fait " + resultatadd + " ou si on multiplie " + resultatmult);
+//alert("Le résultat fait " + .val(nombre1+nombre2) + nombre1*nombre2);
